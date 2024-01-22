3a1c9d6329e196167db4ca002bf61bb2ca0db1dc
Loventia <48926260+Loventia@users.noreply.github.com>
2024-01-22 10:43:44 +0200

    upload frontend

---
+++ client/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/styles/scroll.tsx','./src/assets/Navbar.tsx','./src/HikingSpotsList.tsx', './public/index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
